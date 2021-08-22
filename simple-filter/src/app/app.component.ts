c3e00b70c2f966bf595b42356c022f4838fbce9e
Michal Hrabovský <xhrabovskym@stuba.sk>
2021-08-22 22:43:44 +0200

    3 hours of work

--- simple-filter/src/app/app.component.ts
+++ simple-filter/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+
+export interface eventsStructure {
+  events: string[];
+  eventAttributes: string[]
+  stringOperations: string[]
+  numberOperations: string[]
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'simple-filter';
+  event = new FormControl('');
+  attribute = new FormControl('Unnamed');
+  operation = new FormControl('');
+  filteredEvents!: Observable<string[]>;
+
+
+  attributeAdded: boolean = false;
+  operationType: string = 'string';
+
+  eventsList: eventsStructure = {
+    events: ["first-event", "second-event", "third-event", "fourth-event"],
+    eventAttributes: ["Unnamed", "price", "number of items", "timestamp", "index"],
+    stringOperations: ["equal to", "in between", "less than", "greater than"],
+    numberOperations: ["equals", "does not equal", "contains", "does not contain"]
+  };
+
+
+  ngOnInit() {
+    this.filteredEvents = this.event.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+    console.log(this.event.value);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.eventsList.events.filter(event => this._normalizeValue(event).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toString().toLowerCase().replace(/\s/g, '');
+  }
+
 }
--- simple-filter/src/app/app.module.ts
+++ simple-filter/src/app/app.module.ts
@@ -3,7 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 
 @NgModule({
   declarations: [
@@ -12,6 +21,13 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatCardModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
     NoopAnimationsModule
   ],
   providers: [],
